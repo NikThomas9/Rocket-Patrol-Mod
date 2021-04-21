d1d585782d25e83b5dd18bdab8671b6af16c4b7a
NikThomas9 <nikrthomas@gmail.com>
2021-04-20 20:14:03 -0700

    Final Commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /*
 // Created by Niklas Thomas
-// 
+// Fishing Frenzy - Rocket Patrol Mod
+// 4/20/21
 //////// POINT BREAKDOWN ////////
 // Redesign the Game's Theme (60)
 // New Spaceship Type (20)
@@ -11,6 +12,11 @@
 // Sources: https://phaser.discourse.group/t/is-there-fixedupdate-like-unity-in-phaser/5953
 //          https://freesound.org/people/qubodup/sounds/60013/
 //          https://freesound.org/people/Glaneur%20de%20sons/sounds/104945/
+//
+// Time it took to complete: Asset Replacement (in Aseprite) - 2 hours
+// Timer - 2 hours (research led me to many many misleading debug tests)
+// Parallax - 10 min
+// Tweaking pre-existing assets - 3 hours (moving UI around, changing direction of bullet)
 */
 
 let config = {
